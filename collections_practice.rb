371fc0d0dfd92f7977bf45dd47ff250b1b1f49fd
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-27 01:07:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -79,6 +79,6 @@ def organize_schools(schools)
       new_hash[city] = []
     end
     if !new_hash[city].include?(school)
-      new_hash[city].push()
+      new_hash[city].push(school)
   end
 end
